b6f3e13c827b40be91ae26c8ac29c948f2d69017
John Glenn <john.glenn@intimetec.com>
2022-05-07 11:27:52 -0400

    added thread-safe demo

--- Sequencer.cs
+++ Sequencer.cs
@@ -31,9 +31,46 @@ namespace ThreeNplus1
             int maxCycleLength = 0;
             Parallel.For(start, end, item => {
                 int cycleLength = generator.calculateCycleLength(cycleLengths, item);
+
+                // this operation poses a problem -> multiple threads will be reading and accessing this variable in parallel
+                // .. for both read and write operations; technically, this could result in two parallel operations setting the
+                // .. new max value at essentially the same time causing the incorrect max value to be returned
                 maxCycleLength = Math.Max(maxCycleLength, cycleLength);
             });
             return maxCycleLength;
+        }
+
+        public int calculateMaxCycleLengthThreadSafe()
+        {
+            ConcurrentDictionary<long, int> cycleLengths = new ConcurrentDictionary<long, int>();
+
+            ConcurrentBag<int> maxCyclesLengthsFromThreads = new ConcurrentBag<int>();
+
+            Parallel.For(
+                start, // inclusive start for the index
+                end, // exclusive end for the index
+                () => 0, // the initial value for thisThreadsRunningMax, which we want to be zero
+
+                // below is our parallel action
+                (loopIndex, loopStateThatWeDontUse, thisThreadsRunningMax) => {
+                    int cycleLength = generator.calculateCycleLength(cycleLengths, loopIndex);
+
+                    // NOTE: this value gets stored as thisThreadsRunningMax and passed in to the next run
+                    // There will be several instances of thisThreadsRunningMax - one per working thread.
+                    // The value we set the first time our parallel function runs on a thread will be passed
+                    // .. to the next function run on the same thread.
+                    // thisThreadsRunningMax is thread safe (because there is a separate instance for each thread
+                    return Math.Max(thisThreadsRunningMax, cycleLength);
+                },
+
+                // below is the "finally" action that will be performed in parallel on each result (aka: thisThreadsRunningMax)
+                // .. of each set of parallel actions; this function must be thread safe
+                (eachThreadsRunningMax) => maxCyclesLengthsFromThreads.Add(eachThreadsRunningMax)
+                );
+
+            // now we need to take the few maxCyclesLengthsFromThreads and find the true maximum
+
+            return maxCyclesLengthsFromThreads.Max(); ;
         }
     }
 }
